85bbea7bedf3aeafc552a49c491117d3daa7c287
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2021-12-06 11:19:10 -0600

    Initial commit

--- plugins/start_command_text.py
+++ plugins/start_command_text.py
@@ -25,9 +25,7 @@ bot_start_time = time.time()
 async def start(client, message):
     bot_uptime = time.strftime("%Hh %Mm %Ss", time.gmtime(time.time() - bot_start_time)) 
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("JOIN", url="https://t.me/TGBotsCollection")],
-        [InlineKeyboardButton(
-            "Try", url="https://t.me/TGBotsCollectionbot")]
+        [InlineKeyboardButton("Follow", url="https://github.com/SpEcHlDe")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\nThis is Multipurpose Bot that can perform many functions.\n\n/help for More info \n Bot Uptime : {bot_uptime}"
     await message.reply_text(welcomed, reply_markup=joinButton)
