3380accd63497016b5936e3f20032afcb8317245
SpEcHlDe <89847967+SpEcHlDe@users.noreply.github.com>
2022-02-22 11:21:49 -0600

    Update tg_to_gdrive.py

--- plugins/tg_to_gdrive.py
+++ plugins/tg_to_gdrive.py
@@ -30,74 +30,76 @@ async def tg_to_gdrive_upload(bot, update):
         )
         return
     logger.info(update.from_user)
- try:
-    if update.reply_to_message is None:
-        await bot.send_message(text="Reply to any file", chat_id=update.chat.id, reply_to_message_id=update.message_id)
-    else:
-    download_location = Config.DOWNLOAD_LOCATION + "/"
-    reply_message = await bot.send_message(
-        chat_id=update.chat.id,
-        text=Translation.DOWNLOAD_START,
-        reply_to_message_id=update.message_id
-    )
-    c_time = time.time()
-    the_real_download_location = await bot.download_media(
-        message=update.reply_to_message,
-        file_name=download_location,
-        progress=progress_for_pyrogram,
-        progress_args=(
+    try:
+        if update.reply_to_message is None:
+            await bot.send_message(text="Reply to any file", chat_id=update.chat.id, reply_to_message_id=update.message_id)
+        else:
+        download_location = Config.DOWNLOAD_LOCATION + "/"
+        reply_message = await bot.send_message(
+            chat_id=update.chat.id,
+            text=Translation.DOWNLOAD_START,
+            reply_to_message_id=update.message_id
+        )
+        c_time = time.time()
+        the_real_download_location = await bot.download_media(
+            message=update.reply_to_message,
+            file_name=download_location,
+            progress=progress_for_pyrogram,
+            progress_args=(
             Translation.DOWNLOAD_START,
             reply_message,
             c_time
+            )
         )
-    )
-    if the_real_download_location is not None:
-        try:
-            await bot.edit_message_text(
-                text=Translation.SAVED_RECVD_DOC_FILE,
+        if the_real_download_location is not None:
+            try:
+                await bot.edit_message_text(
+                    text=Translation.SAVED_RECVD_DOC_FILE,
+                    chat_id=update.chat.id,
+                    message_id=reply_message.message_id
+                )
+            except:
+                pass
+        txt = update.text
+        if txt.find("rename") > -1 and len(txt[txt.find("rename") + 7:]) > 0:
+            custom_file_name = txt[txt.find("rename") + 7:]
+            custom_file_name = await sanitize_file_name(custom_file_name)
+            custom_file_name = await sanitize_text(custom_file_name)
+            new_file_name = download_location + custom_file_name
+            os.rename(the_real_download_location, new_file_name)
+            the_real_download_location = new_file_name
+        download_directory = the_real_download_location
+        if os.path.exists(download_directory):
+            end_one = datetime.now()
+            up_name = pathlib.PurePath(download_directory).name
+            size = get_readable_file_size(get_path_size(download_directory))
+            try:
+                await bot.edit_message_text(
+                    text="Download Completed!!!\n Upload in progress",
+                    chat_id=reply_message.chat.id,
+                    message_id=reply_message.message_id
+                )
+            except Exception as e:
+                logger.info(str(e))
+                pass
+            logger.info(f"Upload Name : {up_name}")
+            drive = gdriveTools.GoogleDriveHelper(up_name)
+            gd_url, index_url = drive.upload(download_directory)
+            if Config.INDEX_URL:
+                logger.info(index_url)
+                button = [[InlineKeyboardButton(text="☁️ Drive Link", url=f"{gd_url}"), InlineKeyboardButton(text="⚡️ Index Link", url=f"{index_url}")]]
+            else:
+                button = [[InlineKeyboardButton(text="☁️ Drive Link", url=f"{gd_url}")]]
+            button_markup = InlineKeyboardMarkup(button)
+            await bot.send_message(
+                text=f"🤖: <b>{up_name}</b> has been Uploaded successfully to your Cloud🤒 \n📀 Size: {size}",
                 chat_id=update.chat.id,
-                message_id=reply_message.message_id
-            )
-        except:
-            pass
-    txt = update.text
-    if txt.find("rename") > -1 and len(txt[txt.find("rename") + 7:]) > 0:
-        custom_file_name = txt[txt.find("rename") + 7:]
-        custom_file_name = await sanitize_file_name(custom_file_name)
-        custom_file_name = await sanitize_text(custom_file_name)
-        new_file_name = download_location + custom_file_name
-        os.rename(the_real_download_location, new_file_name)
-        the_real_download_location = new_file_name
-    download_directory = the_real_download_location
-    if os.path.exists(download_directory):
-        end_one = datetime.now()
-        up_name = pathlib.PurePath(download_directory).name
-        size = get_readable_file_size(get_path_size(download_directory))
-        try:
-            await bot.edit_message_text(
-                text="Download Completed!!!\n Upload in progress",
-                chat_id=reply_message.chat.id,
-                message_id=reply_message.message_id
-            )
-        except Exception as e:
-            logger.info(str(e))
-            pass
-        logger.info(f"Upload Name : {up_name}")
-        drive = gdriveTools.GoogleDriveHelper(up_name)
-        gd_url, index_url = drive.upload(download_directory)
-        if Config.INDEX_URL:
-            logger.info(index_url)
-            button = ([InlineKeyboardButton(text="⚡️ Index Link", url=f"{index_url}")])
+                reply_to_message_id=update.message_id,
+                reply_markup=button_markup)
+            await reply_message.delete()
+    except ValueError as err:
+        if update.from_user.name.isspace() or update.from_user.name == "":
+            mention = f'[user](tg://user?id={update.from_user.id})'
         else:
-            button = [([InlineKeyboardButton(text="☁️ Drive Link", url=f"{gd_url}")])]
-        button_markup = InlineKeyboardMarkup(button)
-        await bot.send_message(
-            text=f"🤖: <b>{up_name}</b> has been Uploaded successfully to your Cloud🤒 \n📀 Size: {size}",
-            chat_id=update.chat.id,
-            reply_to_message_id=update.message_id,
-            reply_markup=button_markup)
-        if Config.INDEX_URL:
-            await generate_short_link(reply_message, index_url, up_name)
-        await reply_message.delete()
- except ValueError as err:
-    await bot.send_message(text=update.from_user.mention,'Your download has been stopped due to:\n', err, chat_id=update.chat.id, reply_to_message_id=update.message_id)
+            mention = f'[{update.from_user.first_name}](tg://user?id={update.from_user.id})'
+        await bot.send_message(text=f'{mention} Your download has been stopped due to:\n{err}', chat_id=update.chat.id, reply_to_message_id=update.message_id)
